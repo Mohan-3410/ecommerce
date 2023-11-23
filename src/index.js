ee944cbf8da824ea0edfb175b6613dab96a82c52
mohan_2231 <mahan.singh.tomar1034@gmail.com>
2023-11-23 16:01:41 +0530

    remove strictmode

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <HashRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
@@ -16,5 +15,4 @@ root.render(
         </PersistGate>
       </Provider>
     </HashRouter>
-  </React.StrictMode>
 );
